f63f622e4284e078cd4f923a7bda2eaf417a2a5c
Jamie Epstein <jasepstein@gmail.com>
2019-06-15 14:42:44 -0500

    Initial commit / base file structure

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* The rest of your application */
+      
+      }
+    </React.Fragment>
+  );
+}
+
+export default App;
