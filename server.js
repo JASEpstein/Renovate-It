e0cced6565c2cad8479c5a791eec93df876463f8
Jamie Epstein <jasepstein@gmail.com>
2019-09-13 14:58:32 -0500

    Changes to build scripts to fix deployment.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const routes = require("./routes");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
+
 const users = require('./routes/api/users')
 
 const app = express();
@@ -35,16 +36,26 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '/client/build')));
+// app.use(express.static(path.join(__dirname, '/client/build')));
 
 // An api endpoint that returns a short list of items
 //app.get(APIroutes);
 
 // Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// app.get('*', (req,res) =>{
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 //dotenv.config();
 
